dd3c652806e613a991b728ad9c9b5226995861b9
Gnanesh211 <Karshvarth2112@gmail.com>
2025-09-06 12:55:13 +0530

    message

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,9 @@
 
 
-export function Card({ children, className }) {
-  return <div className={`card ${className || ""}`}>{children}</div>;
-}
+  export function Card({ children, className }) {
+    return <div className={`card ${className || ""}`}>{children}</div>;
+  }
 
-export function CardContent({ children, className }) {
-  return <div className={`card-content ${className || ""}`}>{children}</div>;
-}
+  export function CardContent({ children, className }) {
+    return <div className={`card-content ${className || ""}`}>{children}</div>;
+  }
